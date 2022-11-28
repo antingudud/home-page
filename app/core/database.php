81e9fb9801c8b78468ceb952dbe226fcc28c1856
Aulia Ridho <68772295+antingudud@users.noreply.github.com>
2022-11-28 08:59:50 +0700

    Update database.php
    properly access json elements and return the connection

--- app/core/database.php
+++ app/core/database.php
@@ -8,23 +8,27 @@ class ConnectDB
     private $user;
     private $password;
     private $database;
+    protected $conn;
     function __construct()
     {
-        $this->configFile = file_get_contents("/config/config.json");
+        $this->configFile = file_get_contents(dirname(__DIR__, 1) . "/config/config.json");
         $this->configContent = json_decode($this->configFile);
-        $this->host = "$this->configContent=>host";
-        $this->user = "$this->configContent=>user";
-        $this->password = "$this->configContent=>password";
-        $this->database = "$this->configContent=>database";
+        $this->host = $this->configContent->{'host'};
+        $this->user = $this->configContent->{'user'};
+        $this->password = $this->configContent->{'password'};
+        $this->database = $this->configContent->{'database'};
     }
     function connect()
     {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         try {
-            $db = new mysqli($this->host, $this->user, $this->password, $this->database);
-            $db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+            $this->conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+            $this->conn->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+            $return $this->conn;
         } catch (\mysqli_sql_exception $e) {
             throw new \mysqli_sql_exception($e->getMessage(), $e->getCode());
+        } finally {
+            unset($host, $database, $user, $password);
         }
     }
 }
